55db4f9a9ee302aa590f7a323c106880678f8655
Sipho Mahlangu <21422869@dut4life.ac.za>
2024-10-15 15:36:17 +0200

    Refactor Header component to improve animation and typography

--- src/app/Header/Header.tsx
+++ src/app/Header/Header.tsx
@@ -6,17 +6,25 @@ import { motion } from 'framer-motion';
 const Header = () => {
   // Animation variants for the background
   const backgroundVariants = {
-    hidden: { y: '100%' }, // Start from below the screen
-    visible: { y: '0%', transition: { duration: 1 } }, // Slide up to its position
+    hidden: { y: '100%', opacity: 0 }, // Start from below the screen
+    visible: { y: '0%', opacity: 1, transition: { duration: 1.2 } }, // Slide up to its position
   };
 
   // Animation variants for text
-  const textVariants = {
+  const headingVariants = {
     hidden: { opacity: 0, y: 50 }, // Start below the screen with opacity 0
+    visible: () => ({
+      opacity: 1,
+      y: 0,
+      transition: { delay: 0.8, duration: 0.9 }, // Adding extra delay
+    }),
+  };
+  const textVariants = {
+    hidden: { opacity: 0, y: 0 }, // Start below the screen with opacity 0
     visible: (i: number) => ({
       opacity: 1,
       y: 0,
-      transition: { delay: i * 0.3 + 0.5, duration: 0.5 }, // Adding extra delay
+      transition: { delay: i * 1 + 0.5, duration: 0.9 }, // Adding extra delay
     }),
   };
 
@@ -28,7 +36,7 @@ const Header = () => {
         animate='visible'
         variants={backgroundVariants}
         className='absolute top-0 left-0 w-full h-full bg-cover bg-center'
-        style={{ backgroundImage: "url('/images/header/header.webp')" }}
+        style={{ backgroundImage: "url('/images/header/header.jpg')" }}
       />
 
       {/* Text Container */}
@@ -36,16 +44,16 @@ const Header = () => {
         <motion.h1
           initial='hidden'
           animate='visible'
-          variants={textVariants}
+          variants={headingVariants}
           custom={0} // Index for delay
-          className='text-[2rem] md:text-[3rem] font-brandon font-bold text-white text-center leading-none uppercase flex gap-4 lg:gap-8'
+          className='text-[4.5rem] md:text-[5rem] font-canela font-thin text-white text-center leading-none capitalize flex flex-col lg:flex-row gap-4 lg:gap-8'
         >
           <span>Zamani</span>
           <Image
             src='/images/header/and.svg'
             width='100'
             height='100'
-            className='w-auto h-[2rem] md:h-[3rem]'
+            className='w-auto h-[4rem] md:h-[5rem]'
             alt='And'
           />
           <span>Palesa</span>
@@ -56,7 +64,7 @@ const Header = () => {
           animate='visible'
           variants={textVariants}
           custom={1} // Index for delay
-          className='text-[1.1rem] md:text-[1.5rem] text-white text-center font-brandon mt-4'
+          className='text-[1.3rem] md:text-[1.5rem] text-white text-center font-canela font-thin mt-4'
         >
           Are Getting Married, October 26, 2024
         </motion.p>
