9022a6d50882ee093c78d7f338b9150ac61d9d7a
Sipho Mahlangu <21422869@dut4life.ac.za>
2024-10-16 13:57:41 +0200

    Refactor Dress component to update dress code details and styles

--- src/app/Dress/Dress.tsx
+++ src/app/Dress/Dress.tsx
@@ -5,15 +5,15 @@ import React from 'react';
 const Dress: React.FC = () => {
   return (
     <section className='w-screen flex flex-col items-center my-[4rem]'>
-      {/* Reception Details */}
-      <div className='font-brandon font-bold text-gray-700 text-[1.5rem] md:text-[1.9rem] lg:text-[2.2rem] uppercase my-[4rem]'>
-        <h1 className='transform translate-x-[-25%] '>Dress</h1>
-        <h1 className='transform translate-x-[25%]'>Code</h1>
+      {/* Dress Code */}
+      <div className='w-full px-8 font-canela font-thin text-gray-700 text-[3rem] md:text-[3.9rem] lg:text-[4.5rem] uppercase my-[4rem]'>
+        <h1 className='transform translate-x-[0%] '>Dress</h1>
+        <h1 className='transform translate-x-[20%]'>Code</h1>
       </div>
 
       {/* Dress Code Details */}
-      <div className='font-calibri text-gray-500 text-center w-[90%] md:w-[60%] lg:w-[40%] leading-[1.75rem]'>
-        <p className='text-[1rem] md:text-[1.2rem]'>
+      <div className='font-canela text-gray-500 text-center w-[90%] md:w-[60%] lg:w-[40%] leading-[1.75rem]'>
+        <p className='text-[1.2rem] md:text-[1.2rem]'>
           The suggested Wedding Dress Code is{' '}
           <span className='font-bold'>Boho Chic / Autumn meets Spring</span>
         </p>
