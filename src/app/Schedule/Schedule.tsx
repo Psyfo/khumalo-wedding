02f9615c590d2c7decec07c04481f417f12bd7d4
Sipho Mahlangu <21422869@dut4life.ac.za>
2024-10-19 19:54:54 +0200

    Refactor font styles and image sizes in Reception and Schedule components

--- src/app/Schedule/Schedule.tsx
+++ src/app/Schedule/Schedule.tsx
@@ -5,13 +5,13 @@ const Schedule: React.FC = () => {
   return (
     <section className='w-screen flex flex-col items-center my-[4rem]  overflow-x-hidden'>
       {/* Church Service */}
-      <div className='mr-auto px-8 font-canela font-thin text-gray-700 text-[2.5rem] md:text-[3.9rem] lg:text-[4rem] capitalize my-[4rem]'>
+      <div className='mr-auto px-[2rem] lg:px-[12rem] font-canela font-thin text-gray-700 text-[2.5rem] md:text-[3.9rem] lg:text-[4rem] capitalize my-[4rem]'>
         <h1 className='transform translate-x-[0%] '>Church</h1>
         <h1 className='transform translate-x-[4rem]'>Service</h1>
       </div>
 
       {/* Info Blocks */}
-      <div className='w-full px-8 lg:px-[4rem] flex flex-col lg:flex-row'>
+      <div className='w-full px-8 lg:px-[12rem] flex flex-col lg:flex-row'>
         <div className='lg:w-[60%]'>
           <div className='flex  mb-4'>
             <div className='w-1/3 md:w-1/3 font-calibri text-gray-700 text-[0.7rem] md:text-[1.1rem] leading-[2rem] flex items-start uppercase'>
@@ -68,9 +68,9 @@ const Schedule: React.FC = () => {
         </div>
 
         {/* Church Image */}
-        <div className='w-full max-w-[25rem] h-[25rem] mx-auto lg:my-auto mt-[5rem] flex items-center justify-center'>
+        <div className='w-full w-[20rem] h-[20rem] mx-auto lg:my-auto mt-[5rem] flex items-center justify-center'>
           <Image
-            src='/images/schedule/church1.png'
+            src='/images/schedule/church2.jpg'
             alt='Church'
             className='rounded-full w-full h-full'
             objectFit='cover'
