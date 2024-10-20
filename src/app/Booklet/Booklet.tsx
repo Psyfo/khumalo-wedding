25ecba0bbe4d54f42b4dad08aac97acefb5e56c7
Sipho Mahlangu <21422869@dut4life.ac.za>
2024-10-20 15:15:08 +0200

    Refactor Booklet component to add wedding registry link and create Wedding Registry section

--- src/app/Booklet/Booklet.tsx
+++ src/app/Booklet/Booklet.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 const Booklet: React.FC = () => {
   return (
     <section className='w-screen flex flex-col items-center py-[4rem] pb-[4rem] border border-t-orange-600'>
+      {/* Wedding Booklet Section */}
       <div className='text-center mb-[2rem] px-[2rem]'>
         <h2 className='font-canela font-thin text-gray-800 text-[2.5rem] md:text-[3.5rem] lg:text-[4rem] capitalize mb-[2rem]'>
           Wedding Booklet
@@ -13,20 +14,6 @@ const Booklet: React.FC = () => {
           day! Inside, you’ll find information about the ceremony, reception,
           accommodation options, transportation, and more. Click below to
           download your copy and be fully prepared for the celebration.
-        </p>
-
-        {/* Wedding Registry Link */}
-        <p className='font-calibri text-gray-600 text-[1rem] md:text-[1.2rem] mt-4'>
-          Don’t forget to visit our{' '}
-          <a
-            href='https://khumalowedding.com'
-            target='_blank'
-            rel='noopener noreferrer'
-            className='text-orange-600 hover:underline'
-          >
-            wedding registry
-          </a>{' '}
-          to see how you can contribute to our new chapter!
         </p>
       </div>
 
@@ -51,6 +38,32 @@ const Booklet: React.FC = () => {
       >
         Download Booklet
       </button>
+
+      {/* Wedding Registry Section */}
+      <div className='text-center mt-[4rem] px-[2rem]'>
+        <h2 className='font-canela font-thin text-gray-800 text-[2.5rem] md:text-[3.5rem] lg:text-[4rem] capitalize mb-[2rem]'>
+          Wedding Registry
+        </h2>
+        <p className='font-calibri text-gray-600 text-[1rem] md:text-[1.2rem]'>
+          Family & Friends,
+          <br />
+          Your presence at our wedding is the greatest gift, and more than
+          enough of a present for us. If you would still like to send us
+          something, please click on the link below.
+          <br />
+          <br />
+          Thank you for celebrating with us!
+        </p>
+
+        <a
+          href='https://khumalowedding.com'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='mt-[2rem] inline-block border border-gray-500 text-gray-500 hover:bg-orange-600 hover:text-white font-calibri text-[1.2rem] px-8 py-4 shadow-lg transition-all duration-300'
+        >
+          Visit Our Registry
+        </a>
+      </div>
     </section>
   );
 };
